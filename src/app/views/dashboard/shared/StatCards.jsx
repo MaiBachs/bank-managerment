9548f4d72637dd99334dca195c30faa265db375c
MaiBachs <maibach699@gmail.com>
2023-09-27 11:52:27 +0700

    update

--- src/app/views/dashboard/shared/StatCards.jsx
+++ src/app/views/dashboard/shared/StatCards.jsx
@@ -2,62 +2,62 @@ import { Box, Card, Grid, Icon, IconButton, styled, Tooltip } from '@mui/materia
 import { Small } from 'app/components/Typography';
 
 const StyledCard = styled(Card)(({ theme }) => ({
-  display: 'flex',
-  flexWrap: 'wrap',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '24px !important',
-  background: theme.palette.background.paper,
-  [theme.breakpoints.down('sm')]: { padding: '16px !important' },
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '24px !important',
+    background: theme.palette.background.paper,
+    [theme.breakpoints.down('sm')]: { padding: '16px !important' },
 }));
 
 const ContentBox = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  flexWrap: 'wrap',
-  alignItems: 'center',
-  '& small': { color: theme.palette.text.secondary },
-  '& .icon': { opacity: 0.6, fontSize: '44px', color: theme.palette.primary.main },
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    '& small': { color: theme.palette.text.secondary },
+    '& .icon': { opacity: 0.6, fontSize: '44px', color: theme.palette.primary.main },
 }));
 
 const Heading = styled('h6')(({ theme }) => ({
-  margin: 0,
-  marginTop: '4px',
-  fontSize: '14px',
-  fontWeight: '500',
-  color: theme.palette.primary.main,
+    margin: 0,
+    marginTop: '4px',
+    fontSize: '14px',
+    fontWeight: '500',
+    color: theme.palette.primary.main,
 }));
 
 const StatCards = () => {
-  const cardList = [
-    { name: 'New Leads', amount: 3050, icon: 'group' },
-    { name: 'This week Sales', amount: '$80,500', icon: 'attach_money' },
-    { name: 'Inventory Status', amount: '8.5% Stock Surplus', icon: 'store' },
-    { name: 'Orders to deliver', amount: '305 Orders', icon: 'shopping_cart' },
-  ];
+    const cardList = [
+        { name: 'Tổng khách hàng', amount: 3050, icon: 'group' },
+        { name: 'Tổng tiền giao dịch nội bộ', amount: '$80,500', icon: 'attach_money' },
+        { name: 'Tổng tiền giao dịch liên ngân hàng', amount: '8.5% Stock Surplus', icon: 'store' },
+        { name: 'Khách hàng đăng kí thẻ trong tháng', amount: '305 Orders', icon: 'shopping_cart' },
+    ];
 
-  return (
-    <Grid container spacing={3} sx={{ mb: '24px' }}>
-      {cardList.map((item, index) => (
-        <Grid item xs={12} md={6} key={index}>
-          <StyledCard elevation={6}>
-            <ContentBox>
-              <Icon className="icon">{item.icon}</Icon>
-              <Box ml="12px">
-                <Small>{item.name}</Small>
-                <Heading>{item.amount}</Heading>
-              </Box>
-            </ContentBox>
+    return (
+        <Grid container spacing={3} sx={{ mb: '24px' }}>
+            {cardList.map((item, index) => (
+                <Grid item xs={12} md={6} key={index}>
+                    <StyledCard elevation={6}>
+                        <ContentBox>
+                            <Icon className="icon">{item.icon}</Icon>
+                            <Box ml="12px">
+                                <Small>{item.name}</Small>
+                                <Heading>{item.amount}</Heading>
+                            </Box>
+                        </ContentBox>
 
-            <Tooltip title="View Details" placement="top">
-              <IconButton>
-                <Icon>arrow_right_alt</Icon>
-              </IconButton>
-            </Tooltip>
-          </StyledCard>
+                        <Tooltip title="View Details" placement="top">
+                            <IconButton>
+                                <Icon>arrow_right_alt</Icon>
+                            </IconButton>
+                        </Tooltip>
+                    </StyledCard>
+                </Grid>
+            ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+    );
 };
 
 export default StatCards;
